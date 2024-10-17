3e9e03941889fb258f94676b81a3dcc5e599df5a
marcus-obeso <marcus.sab.costa@escola.pr.gov.br>
2024-10-17 11:30:01 -0300

    ARRAY

---
+++ Array01/script.js
@@ -0,0 +1,15 @@
+//variáveis unitárias 
+const nome = 'jonas'
+const nome2 = 'julia'
+const nome3 = 'marcos'
+
+
+
+
+
+//variáveis de arrays
+const nomes = ['jonas', 'julia', 'marcos']
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = "dino"
+console.log(nomes[10])
